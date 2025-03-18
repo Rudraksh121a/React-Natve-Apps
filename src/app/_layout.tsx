47af53b33d02e72ee2ff55b7ffca6308cfb41ad1
Rudraksh Kanungo <Rudraksh121a>
2025-03-19 00:01:33 +0530

    Stack navigator

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+      }}
+    >
+    <Stack.Screen name="index" options={{title:"rudra"}}/>
+    </Stack>
+  );
+}
