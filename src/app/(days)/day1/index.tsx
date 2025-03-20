06ea29039cff65139090a91a0c1d46760166b1f3
Rudraksh Kanungo <Rudraksh121a>
2025-03-20 15:42:37 +0530

    Markdown added in day 3

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -6,11 +6,13 @@ const DayDetailsScreen = () => {
   return (
     <View>
       <Stack.Screen options={{title:"Day1"}}/>
-      <Text style={{fontFamily:"AmaticBold",fontSize:100}}>Day Details Screen</Text>
+      <Text style={{fontFamily:"AmaticBold",fontSize:100}}>Create Splesh Screen and set up navigation</Text>
     </View>
   )
 }
 
 export default DayDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+ 
+})
